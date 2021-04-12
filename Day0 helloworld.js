415ad7cce635f3f397849b980e503ffbcfa38f67
manish0502 <manishgoswami495@gmail.com>
2021-04-12 11:07:55 +0530

    Day0: Hello World

---
+++ Day0 helloworld.js
@@ -0,0 +1,7 @@
+function greeting(name) {
+    // This line prints 'Hello, World!' to the console:
+    console.log('Hello, World!');
+    console.log(name);
+    // Write a line of code that prints parameterVariable to stdout using console.log:
+    
+}
